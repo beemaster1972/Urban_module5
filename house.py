c37c90f316e5b0eb335e372ae5fd4faf4fe7cb0e
Дмитрий Янно <ugor.finno@yandex.ru>
2024-12-09 18:05:44 +0500

    modify class House and create module 5 hw 3 Operator overloading

--- house.py
+++ house.py
@@ -4,6 +4,61 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
+    @staticmethod
+    def get_other(other):
+        return other.number_of_floors if isinstance(other,House) else other
+
+    def __eq__(self, other):
+        num_of_floor = self.get_other(other)
+        return self.number_of_floors == num_of_floor
+
+    def __lt__(self, other):
+        num_of_floor = self.get_other(other)
+        return self.number_of_floors < num_of_floor
+
+    def __le__(self, other):
+        num_of_floor = self.get_other(other)
+        return self.number_of_floors <= num_of_floor
+
+    def __gt__(self, other):
+        num_of_floor = self.get_other(other)
+        return self.number_of_floors > num_of_floor
+
+    def __ge__(self, other):
+        num_of_floor = self.get_other(other)
+        return self.number_of_floors >= num_of_floor
+
+    def __ne__(self, other):
+        num_of_floor = self.get_other(other)
+        return self.number_of_floors != num_of_floor
+    @staticmethod
+    def check(value):
+        return value if isinstance(value,int) else 0
+
+    def __add__(self, value):
+        num_of_floor = self.check(self.get_other(value))
+        self.number_of_floors += num_of_floor
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __sub__(self, value):
+        num_of_floor = self.check(self.get_other(value))
+        self.number_of_floors -= num_of_floor
+        return self
+
+    def __rsub__(self, value):
+        num_of_floor = self.check(self.get_other(value))
+        self.number_of_floors = num_of_floor - self.number_of_floors
+        return self
+
+    def __isub__(self, value):
+        return self.__sub__(value)
+
     def __len__(self):
         return self.number_of_floors
 
