c72eebd12cb4f268714512b5b661bc3051cd25fa
Дмитрий Янно <ugor.finno@yandex.ru>
2024-12-09 17:31:32 +0500

    modify class House and create module 5 hw 2 Special class methods

--- house.py
+++ house.py
@@ -4,6 +4,11 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def go_to(self,new_floor):
         if new_floor < 1 or new_floor > self.number_of_floors:
